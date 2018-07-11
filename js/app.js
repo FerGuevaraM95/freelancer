b5f8deb6f88887cdc82bc04821202fb2608274dd
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-07-10 21:29:28 -0500

    Add portfolio

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// EventListener
+document.addEventListener('DOMContentLoaded', () => {
+    loadPortfolio();
+});
+
+// Functions
+function loadPortfolio() {
+    fetch('data.json')
+    .then( response => {
+        return response.json();
+    })
+    .then( data => {
+        let html = '';
+
+        data.portfolio.forEach( portfolio => {
+
+            // Crear el template
+            html += `
+                <div class="element">
+                    <img src="../img/${portfolio.id}.jpg">
+                    <div class="content">
+                        <h3>${portfolio.name}</h3>
+                        <p>${portfolio.desc}</p>
+                    </div>
+                </div>
+            `;
+        });
+
+        // Inyectar el html
+        document.querySelector('#list').innerHTML = html;
+    } )
+}
